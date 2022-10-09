292309a7e8b29e09d7a7bedbe7e6371ddb808e91
Carlos del Blanco <delblanco.carlos@gmail.com>
2022-10-09 17:19:03 +0200

    Change fillGrid function in order to set specific cells alive

--- public/js/FillGrid/fillGrid.js
+++ public/js/FillGrid/fillGrid.js
@@ -1,18 +1,11 @@
 // import createGrid from "../CreateElement/createGrid.js";
 
 function fillGrid(grid) {
-  const newGrid = grid;
-  for (let iIndex = 0; iIndex < grid.length; iIndex++) {
-    for (let jIndex = 0; jIndex < grid.length; jIndex++) {
-      const randomCellStatus = Math.round(Math.random());
-      let cellStatus = false;
-      if (randomCellStatus === 1) {
-        cellStatus = true;
-      }
-      newGrid[iIndex][jIndex].isAlive = cellStatus;
-    }
-  }
-  return newGrid;
+  grid[1][0].isAlive = true;
+  grid[1][1].isAlive = true;
+  grid[1][2].isAlive = true;
+
+  return grid;
 }
 
 export default fillGrid;
--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,5 @@
+import createGrid from "./CreateElement/createGrid.js";
+import fillGrid from "./FillGrid/fillGrid.js";
 
+const newGrid = createGrid(3);
+fillGrid(newGrid);

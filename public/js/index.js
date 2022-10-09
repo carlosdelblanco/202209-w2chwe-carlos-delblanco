43fba2074666b18a091a1c6cfb791d24aa46ae96
Carlos del Blanco <delblanco.carlos@gmail.com>
2022-10-09 12:24:53 +0200

    Create function to Set random alive values for each array cell

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,1 @@
-import CreateElement from "./CreateElement/CreateElement.js";
-import createGrid from "./CreateElement/createGrid.js";
-import fillGrid from "./FillGrid/fillGrid.js";
 
-const randomFilledGrid = fillGrid(newGrid);
-console.table(randomFilledGrid);
